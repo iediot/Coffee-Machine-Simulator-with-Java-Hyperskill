b580f6fdce8db709acd4cb911e1cbc84abc79a87
Tabîrcă Nicolae-Eduard <eduardnicolae092@gmail.com>
2025-07-29 16:47:08 +0300

    Add files via upload

---
+++ machine/CoffeeMachineState.java
@@ -0,0 +1,12 @@
+package machine;
+
+public class CoffeeMachineState {
+    int water, milk, beans, cups, money;
+    public CoffeeMachineState (int water, int milk, int beans, int cups, int money) {
+        this.water = water;
+        this.milk = milk;
+        this.beans = beans;
+        this.cups = cups;
+        this.money = money;
+    }
+}
